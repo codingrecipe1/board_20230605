ada6cfcb2a790e225dbc1abf6425e54953bd4c66
codingrecipe1 <codingrecipe1@gmail.com>
2023-06-08 15:27:38 +0900

    06_댓글_완료

--- src/main/java/com/icia/board/controller/BoardController.java
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.icia.board.controller;
 
 import com.icia.board.dto.BoardDTO;
+import com.icia.board.dto.CommentDTO;
 import com.icia.board.service.BoardService;
+import com.icia.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ import java.util.NoSuchElementException;
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
+    private final CommentService commentService;
 
     @GetMapping("/save")
     public String saveForm() {
@@ -40,14 +43,20 @@ public class BoardController {
     @GetMapping("/{id}")
     public String findById(@PathVariable Long id, Model model) {
         boardService.updateHits(id);
-        BoardDTO boardDTO = null;
         try {
-            boardDTO = boardService.findById(id);
+            BoardDTO boardDTO = boardService.findById(id);
+            model.addAttribute("board", boardDTO);
+            List<CommentDTO> commentDTOList = commentService.findAll(id);
+            if (commentDTOList.size() > 0) {
+                model.addAttribute("commentList", commentDTOList);
+            } else {
+                model.addAttribute("commentList", null);
+            }
+            return "boardPages/boardDetail";
         } catch (NoSuchElementException e) {
             return "boardPages/boardNotFound";
         }
-        model.addAttribute("board", boardDTO);
-        return "boardPages/boardDetail";
+
     }
 
     @DeleteMapping("/{id}")
